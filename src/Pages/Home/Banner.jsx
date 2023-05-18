eb23ce5dac0fecd2e60d1f283c8db7d0ac08b016
ariful-islam <arifulislam204909@gmail.com>
2023-05-19 01:06:14 +0600

    AOS package added in gallery section

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,10 +1,10 @@
-import img1 from "../../assets/images/img1.jpg"
-import img2 from "../../assets/images/img2.jpg"
-import img3 from "../../assets/images/img3.jpg"
-import img4 from "../../assets/images/img4.jpg"
+import img1 from "../../assets/images/img1.jpg";
+import img2 from "../../assets/images/img2.jpg";
+import img3 from "../../assets/images/img3.jpg";
+import img4 from "../../assets/images/img4.jpg";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init();
 
@@ -12,21 +12,27 @@ const Banner = () => {
   return (
     <div className="carousel w-full h-[600px] mt-20">
       <div id="slide1" className="carousel-item relative w-full ">
-        <img src={img1} className="w-full rounded-xl"/>
+        <img
+          src={img1}
+          className="w-full rounded-xl"
+          data-aos="fade-left"
+          data-aos-offset="300"
+          data-aos-easing="ease-in-sine"
+        />
 
         <div className="absolute rounded-xl flex items-center pl-16 h-full top-0 left-0 right-5 bottom-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) 100%)]">
           <div className="text-white space-y-7 w-1/2">
-            <h2 className="text-7xl font-bold" >
-             Best Kids store.
-            </h2>
+            <h2 className="text-7xl font-bold">Best Kids store.</h2>
             <h3 className="text-4xl font-bold">
-            Electric Powered Kids Ride on Car Toy - yellow
+              Electric Powered Kids Ride on Car Toy - yellow
             </h3>
             <p>
-            2.4G remote control function; slow start and stop; forward and reverse; scissors door; four-wheel shock absorber; pneumatic rod; spoke wheel; 3-point seat belt; front and rear lights; horn; start sound.
+              2.4G remote control function; slow start and stop; forward and
+              reverse; scissors door; four-wheel shock absorber; pneumatic rod;
+              spoke wheel; 3-point seat belt; front and rear lights; horn; start
+              sound.
             </p>
             <div>
-
               <button className="btn btn-outline btn-secondary">
                 Explore More
               </button>
@@ -48,20 +54,18 @@ const Banner = () => {
 
         <div className="absolute rounded-xl flex items-center pl-16 h-full top-0 left-0 right-5 bottom-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) 100%)]">
           <div className="text-white space-y-7 w-1/2">
-            <h2 className="text-6xl font-bold">
-            Best Kids store.
-            </h2>
+            <h2 className="text-6xl font-bold">Best Kids store.</h2>
             <h3 className="text-7xl font-bold">
-            Racing Baby Big Electric Car Jeep
+              Racing Baby Big Electric Car Jeep
             </h3>
             <p>
-            The Ride on Jeep can be controlled by dual control modes. Parent can help your children control this jeep with 2.4G wireless remote controller
+              The Ride on Jeep can be controlled by dual control modes. Parent
+              can help your children control this jeep with 2.4G wireless remote
+              controller
             </p>
             <div>
-
-
               <button className="btn btn-outline btn-secondary">
-               Explore More
+                Explore More
               </button>
             </div>
           </div>
@@ -81,19 +85,19 @@ const Banner = () => {
 
         <div className="absolute flex rounded-xl items-center pl-16 h-full top-0 left-0 right-5 bottom-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) 100%)]">
           <div className="text-white space-y-7 w-1/2">
-          <h2 className="text-7xl font-bold">
-            Best Kids store.
-            </h2>
+            <h2 className="text-7xl font-bold">Best Kids store.</h2>
             <h3 className="text-4xl font-bold">
-            The Ferrari Battery Operated Ride On Car for Kids 
+              The Ferrari Battery Operated Ride On Car for Kids
             </h3>
             <p>
-            this lookalike la ferrari ride-on toy car is easy to clean. Assembly required. Suitable for kids between 2 to 6 years of age and has a maximum weight capacity of 30kgs. Charge the battery according to the included instruction manual
+              this lookalike la ferrari ride-on toy car is easy to clean.
+              Assembly required. Suitable for kids between 2 to 6 years of age
+              and has a maximum weight capacity of 30kgs. Charge the battery
+              according to the included instruction manual
             </p>
             <div>
-
               <button className="btn btn-outline btn-secondary">
-               Explore More
+                Explore More
               </button>
             </div>
           </div>
@@ -113,19 +117,14 @@ const Banner = () => {
 
         <div className="absolute rounded-xl flex items-center pl-16 h-full top-0 left-0 right-5 bottom-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) 100%)]">
           <div className="text-white space-y-6 w-1/2">
-            <h2 className="text-7xl font-bold">
-                Best Kids Store.
-            
-            </h2>
-            <h3 className="text-4xl font-bold">
-            Off Roader Kids Jeep
-            </h3>
+            <h2 className="text-7xl font-bold">Best Kids Store.</h2>
+            <h3 className="text-4xl font-bold">Off Roader Kids Jeep</h3>
             <p>
-            R/C Electric Motor With
-            Rechargeable Battery Operated Ride On Jeep For Kids.
+              R/C Electric Motor With Rechargeable Battery Operated Ride On Jeep
+              For Kids.
             </p>
             <div>
-            <button className="btn btn-outline btn-secondary">
+              <button className="btn btn-outline btn-secondary">
                 Explore More
               </button>
             </div>
