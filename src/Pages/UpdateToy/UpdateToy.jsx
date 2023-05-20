6a25de87bc6567708395e2c0a4e57da5d92af36d
ariful-islam <arifulislam204909@gmail.com>
2023-05-20 19:01:47 +0600

    update data from my toy route

--- src/Pages/UpdateToy/UpdateToy.jsx
+++ src/Pages/UpdateToy/UpdateToy.jsx
@@ -1,17 +1,92 @@
-
 import { useLoaderData } from "react-router-dom";
 
-
 const UpdateToy = () => {
-    const update =useLoaderData();
-    const {price}=update;
-    
-    return (
-        <div className="text-center">
-            <h2>hello: {price}</h2>
-            
+  const update = useLoaderData();
+  const { price, quantity, description, _id } = update;
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    const form = event.target;
+
+    const price = form.price.value;
+    const quantity = form.quantity.value;
+    const description = form.description.value;
+
+    const update = {
+      price,
+      quantity,
+      description,
+      _id,
+    };
+
+    console.log(update);
+
+    // update in to database
+    fetch(`http://localhost:5000/updateData/${_id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(update),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <form onSubmit={handleUpdate}>
+        <div className="card-body">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Price</span>
+              </label>
+              <input
+                name="price"
+                type="text"
+                defaultValue={price}
+                className="input input-bordered input-primary"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Available Quantity</span>
+              </label>
+              <input
+                name="quantity"
+                type="text"
+                defaultValue={quantity}
+                className="input input-bordered input-primary"
+              />
+            </div>
+          </div>
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Details Description</span>
+            </label>
+            <textarea
+              name="description"
+              type="text"
+              defaultValue={description}
+              className="textarea textarea-primary"
+              placeholder="Type here....."
+            ></textarea>
+          </div>
+          <div className="form-control mt-6">
+            <input
+              className="btn btn-primary btn-block"
+              type="submit"
+              value="Update"
+            />
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default UpdateToy;
